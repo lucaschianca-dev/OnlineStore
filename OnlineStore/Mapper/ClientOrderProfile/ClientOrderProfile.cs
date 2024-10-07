3c325bfeb6be20efae14e9d4eb9053ee7b69b97e
Lucas Chianca <lucaseduardochianca.dev@gmail.com>
2024-10-07 17:49:17 -0300

    update: mapeamento de ClientOrderInput para ClientOrder atualizado para ignorar TotalAmount, Items e Status, pois sao atribuidos manualmente

--- OnlineStore/Mapper/ClientOrderProfile/ClientOrderProfile.cs
+++ OnlineStore/Mapper/ClientOrderProfile/ClientOrderProfile.cs
@@ -9,7 +9,10 @@ public class ClientOrderProfile: Profile
     public ClientOrderProfile()
     {
         // Mapeamento de CreateClientOrderInput para ClientOrder
-        CreateMap<AddClientOrderInput, ClientOrder>();
+        CreateMap<AddClientOrderInput, ClientOrder>()
+            .ForMember(dest => dest.TotalAmount, opt => opt.Ignore()) // Ignorar TotalAmount, pois ele é calculado manualmente
+            .ForMember(dest => dest.Items, opt => opt.Ignore()) // Ignorar Items, pois eles são mapeados manualmente
+            .ForMember(dest => dest.Status, opt => opt.Ignore()); // Ignorar Status, pois você atribui manualmente
 
         // Mapeamento de ClientOrderService para AddClientOrderOutput
         CreateMap<ClientOrder, AddClientOrderOutput>()
