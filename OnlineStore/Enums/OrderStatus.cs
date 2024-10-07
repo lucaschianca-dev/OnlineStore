34b60f806f79d9f56c3b28e2cddde21d2396aa67
Lucas Chianca <lucaseduardochianca.dev@gmail.com>
2024-10-07 17:47:30 -0300

    feat: criado Enum para trabalhar com status do ClientOrder

---
+++ OnlineStore/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineStore.Enums;
+
+public enum OrderStatus
+{
+    PENDING,          
+    AWAITING_PAYMENT,  
+    PAYMENT_FAILED,    
+    APPROVED,         
+    PROCESSING,       
+    SHIPPED,         
+    DELIVERED,        
+    CANCELLED,        
+    REFUNDED,         
+    RETURNED
+}
