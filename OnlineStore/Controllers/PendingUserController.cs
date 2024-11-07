00a9b3009ae7eb0965182801238d49b7a962db31
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2024-11-07 14:44:25 -0300

    feat: adicionado PendingUserService e PendingUserControoler. Removido AdminController e PendingUserCleanupService

---
+++ OnlineStore/Controllers/PendingUserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Services.PendingUserService;
+
+namespace OnlineStore.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PendingUserController : ControllerBase
+{
+    private readonly PendingUserService _pendingUserService;
+
+    public PendingUserController(PendingUserService pendingUserService)
+    {
+        _pendingUserService = pendingUserService;
+    }
+
+    [HttpDelete("cleanup-pending-users")]
+    public async Task<IActionResult> CleanupPendingUsers()
+    {
+        await _pendingUserService.DeleteOldPendingUsersAsync(7);
+        return Ok("Pending users older than 7 days have been cleaned up.");
+    }
+
+    [HttpGet("older-than")]
+    public async Task<IActionResult> GetPendingUsersOlderThan()
+    {
+        var pendingUsers = await _pendingUserService.GetPendingUsersOlderThanAsync(7);
+        return Ok(pendingUsers);
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllPendingUsers()
+    {
+        var pendingUsers = await _pendingUserService.GetAllPendingUsersAsync();
+        return Ok(pendingUsers);
+    }
+}
+
---
+++ OnlineStore/Services/PendingUserService/PendingUserService.cs
@@ -0,0 +1,36 @@
+﻿using OnlineStore.Models;
+using OnlineStore.Repositories.PendingUserRepository;
+
+namespace OnlineStore.Services.PendingUserService;
+
+public class PendingUserService
+{
+    private readonly IPendingUserRepository _pendingUserRepository;
+
+    public PendingUserService(IPendingUserRepository pendingUserRepository)
+    {
+        _pendingUserRepository = pendingUserRepository;
+    }
+
+    public async Task DeleteOldPendingUsersAsync(int days = 7)
+    {
+        var oldPendingUsers = await _pendingUserRepository.GetPendingUsersOlderThanAsync(days);
+
+        foreach (var pendingUser in oldPendingUsers)
+        {
+            await _pendingUserRepository.DeletePendingUserAsync(pendingUser.Id);
+            Console.WriteLine($"PendingUser com email {pendingUser.Email} removido por inatividade.");
+        }
+    }
+
+    public async Task<List<PendingUser>> GetPendingUsersOlderThanAsync(int days)
+    {
+        return await _pendingUserRepository.GetPendingUsersOlderThanAsync(days);
+    }
+
+    public async Task<List<PendingUser>> GetAllPendingUsersAsync()
+    {
+        return await _pendingUserRepository.GetAllPendingUsersAsync();
+    }
+}
+
